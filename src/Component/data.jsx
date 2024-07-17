8b9cc3b0c511d8bab035e08949dc29a0ce150fb6
Immortalsoul21 <tusharmondal05032003@gmail.com>
2024-07-17 15:58:34 +0530

    updated

--- src/Component/data.jsx
+++ src/Component/data.jsx
@@ -1,18 +1,22 @@
+ import todoimg from "../../dist/assets/Firefly todo application 66840.jpg"
+ import erpimg from "../../dist/assets/Firefly enterprise resource planning application 66840.jpg"
+ import pglifeimg from "../../dist/assets/Firefly PG searching website 57381.jpg"
+ 
  const data = [
     {
         id: 1,
         link: "https://github.com/Immortalsoul21/TodoUsingMern.git",
-        img: "../../src/assets/Firefly todo application 66840.jpg",
+        img: {todoimg},
     },
     {
         id: 2,
         link: "https://github.com/Immortalsoul21/fullstackbasicERP.git",
-        img: "../../src/assets/Firefly enterprise resource planning application 66840.jpg",
+        img: {erpimg},
     },
     {
         id: 3,
         link: "https://github.com/Immortalsoul21/PGLIFE.git",
-        img: "../../src/assets/Firefly PG searching website 57381.jpg",
+        img: {pglifeimg},
     }
 
     
